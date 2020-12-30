167f1dc73a5a42ab5e272dd3a9c1da767643a4cb
Abdulsalam Ibrahim <engineerabdulsalam0@gmail.com>
2020-12-30 17:30:24 +0100

    replaced older images with optimized images

--- src/content/project.js
+++ src/content/project.js
@@ -4,21 +4,21 @@ const projectData = [
         info: 'An app to check latest covid-19 news and cases around the world',
         techstack: 'React Native',
         github: 'https://github.com/snazzyai/covid-19-tracker',
-        imageUrl: 'https://dc721.4shared.com/img/o1NNZK-Rea/s24/1763025fa60/Screenshot_20201204-225215?async&rand=0.28017643084473054'
+        imageUrl: 'https://dc703.4shared.com/img/WPZjItXvea/s24/176b47583a8/tracker?async&rand=0.8119435334856187'
     },
     {
         name: 'Sharpgas',
         info: 'Official website of Sharpgas',
         techstack: 'Angular',
         github: 'https://github.com/snazzyai/sharpgas',
-        imageUrl: 'https://dc721.4shared.com/img/h2tprIxdea/s24/17630a0c010/gas?async&rand=0.8478135106640585'
+        imageUrl: 'https://dc703.4shared.com/img/aG_rGstfiq/s24/176b4758790/sharp?async&rand=0.6783339795959793'
     },
     {
         name: 'Tracker',
         info: 'Tracker Mobile App for tracking activity with Bangle JS smartwatch',
         techstack: 'React Native, Javascript(BangleJS)',
         github: 'https://github.com/snazzyai/TrackerMobile',
-        imageUrl: 'https://dc721.4shared.com/img/SwbOCZnwiq/s24/176309cff20/Screenshot_20200730-135701?async=&rand=0.9191261572776743'
+        imageUrl: 'https://dc703.4shared.com/img/coE7lQnaea/s24/176b4758790/covid?async&rand=0.07477220804914841'
     },
     
 ]
