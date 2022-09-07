9641652c4302bda8d60c0865ec70a17d17b4e978
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-09-07 17:04:09 +0200

    removed styles

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,44 +1,33 @@
-import React, {useState, useEffect} from "react"
-import * as skillStyles from './skills.module.sass'
-import { motion } from 'framer-motion'
+import React from "react"
+import * as skillStyles from "./skills.module.sass"
+import { motion } from "framer-motion"
 
-
-
-const Skills = ({skillData}) => {
-
-    // const [skills, setSkills] = useState([])
-
-    // useEffect(()=>{
-    //     setSkills(skillData)
-    //   }, [])
-
-      return (
-        <div className={skillStyles.main}>
-        <h1  className={skillStyles.heading}>Skills</h1>
-        <div className={skillStyles.container}>
-        {
-        
-          skillData && skillData.map((skill)=>{
-            return(
-              <motion.div 
+const Skills = ({ skillData }) => {
+  return (
+    <div>
+      <h1 className={skillStyles.heading}>Skills</h1>
+      <div className={skillStyles.container}>
+        {skillData &&
+          skillData.map(skill => {
+            return (
+              <motion.div
                 initial={{ opacity: 0 }}
                 whileInView={{ opacity: 1 }}
                 transition={{
                   type: "spring",
                   damping: 10,
                   mass: 0.75,
-                  stiffness: 100,}}
-                  className={skillStyles.skill}>
+                  stiffness: 100,
+                }}
+                className={skillStyles.skill}
+              >
                 {skill}
               </motion.div>
             )
-          })
-        }
-
+          })}
       </div>
-      </div>
-      )
-
+    </div>
+  )
 }
 
 export default Skills
