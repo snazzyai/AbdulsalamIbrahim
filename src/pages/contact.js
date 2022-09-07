3af829a7ced00c67d2c366e64552a6a161489f09
abdulsalamibrahim <abdulsalamibrahim7@gmail.com>
2022-09-07 17:03:58 +0200

    removed unncecessary component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,11 @@
 import React from "react"
-import Layout from "../components/Layout/Layout"
 import "../styles/index.sass"
-
-
 
 const Contact = () => {
   return (
-    <Layout>
-      <div className="main">
-        <h1>Oops.. Under Construction! </h1>
-      </div>
-    </Layout>
+    <div className="main">
+      <h1>Oops.. Under Construction! </h1>
+    </div>
   )
 }
 
