b6cb153044039c3d15c9d2ddc46076aa6105b4e2
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2023-09-12 15:06:30 +0200

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,6 +5,7 @@ import {
   faDownload
 } from "@fortawesome/free-solid-svg-icons"
 import { motion } from 'framer-motion'
+import profilesummary from '../../content/profilesummary.js'
 
 const AboutMe = () => {
   return (
@@ -14,15 +15,7 @@ const AboutMe = () => {
         <div className={aboutStyles.aboutView}>
           <div className={aboutStyles.aboutText}>
             <p>
-              Abdulsalam Ibrahim is a software engineeer with more than 3
-              years of experience in Software Development and Cross Platform Mobile
-              Application Development. Abdulsalam is a
-              technology enthusiast with flare for developing usable solutions
-              with focus on code readability and performance. He is business-focused, communicative
-              and a team player with good leadership skills.
-              He is also an advocate
-              of open source contribution with a passion to keep learning and using new technologies technologies
-              profer solutions effectively.
+              { profilesummary }
             </p>
             <p>
              
@@ -40,7 +33,7 @@ const AboutMe = () => {
                 icon={faDownload}
               />
                  <a
-                  href="https://drive.google.com/file/d/16Wh123T4LkBMBiw8dPycQaR3YF6RSQSw/view?usp=drive_link"
+                  href="https://drive.google.com/file/d/1bRfC0sD-UVz3yrNW8f7WX-_EMjL7HjJn/view?usp=sharing"
                   className={aboutStyles.buttonTwoLink}
                   download
                 >
