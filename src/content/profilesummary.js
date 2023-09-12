4cdcab51c8baa147002c2ceb45ef7ec9c136d48c
Abdulsalam Ibrahim <abdulsalamibrahim7@gmail.com>
2023-09-12 15:08:16 +0200

    Create profilesummary.js

---
+++ src/content/profilesummary.js
@@ -0,0 +1,10 @@
+const profilesummary = "
+    Experienced software developer with a versatile skill set encompassing mobile,
+frontend, and backend development. Proficient in designing and implementing
+robust applications for various platforms, with a keen eye for user-centric design and
+seamless functionality. Adept at leveraging a wide range of technologies to deliver
+scalable solutions that meet client objectives and drive exceptional user experiences.
+"
+
+
+export default profilesummary 
